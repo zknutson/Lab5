f3c6270089511269233472c4082bec1ee78bb130
z <z.knutson@ufl.edu>
2023-10-19 16:35:00 -0400

    Init

---
+++ Data Reduction.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import glob
+
+# Importing the data
+# # The data is in the form of a .txt file
+# The data is in the form of a 2D array
+
+
